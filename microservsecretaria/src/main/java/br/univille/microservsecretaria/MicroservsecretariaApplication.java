1d90035ab466723a8e42cc29ba2f42c12558a98e
Vinícius Wille <130087613+willevini@users.noreply.github.com>
2024-10-03 00:16:10 +0000

    start project

---
+++ microservsecretaria/src/main/java/br/univille/microservsecretaria/MicroservsecretariaApplication.java
@@ -0,0 +1,13 @@
+package br.univille.microservsecretaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservsecretariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservsecretariaApplication.class, args);
+	}
+
+}
